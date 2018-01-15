5a35da395c5bc1bb268345691be3afca9f314ca8
JohnMrziglod <mrzo@gmx.de>
2018-01-15 20:06:39 +0100

    Replace ArrayGroup by xarray

--- monitor.py
+++ monitor.py
@@ -12,39 +12,54 @@ This script can create two types of plots:
 
 import argparse
 import logging
-import os.path
-import traceback
 import warnings
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import xarray as xr
 
 import cloud
 
 
-logger = logging.getLogger(__name__)
-
-
-def get_bin_size(data_len, config):
-    """Helper function to get the bin size for averaging.
+def sample(dataset, config, start, end, fields=None):
+    """Helper function to get a sample from the data and change its time
+    resolution.
 
     Args:
-        data_len: The length of a data array.
+        dataset: A Dataset object.
         config: A dictionary-like object with configuration keys.
+        start: Start time of the plot
+        end: End time of the plot
+        fields: Fields that you want to extract.
 
     Returns:
         An integer.
     """
-    # Zero means no averaging
-    if int(config["Plots"]["overview_max_points"]) == 0:
-        return 1
+    if fields is None:
+        reader_args = {}
+    else:
+        reader_args = {
+            "fields": fields,
+        }
 
-    bin_size = data_len / int(config["Plots"]["overview_max_points"])
-    if bin_size < 1:
-        return 1
+    data = xr.concat(
+        dataset.read_period(start, end, **reader_args),
+        dim="time",
+    )
+    data = data.sortby("time")
+    data = data.sel(time=slice(start, end))
 
-    return int(bin_size)
+    if (end - start) > pd.Timedelta("7 days"):
+        new_resolution = config["Plots"]["weekly_plots_average"]
+    elif (end - start) > pd.Timedelta("24 hours"):
+        new_resolution = config["Plots"]["daily_plots_average"]
+    else:
+        new_resolution = config["Plots"]["hourly_plots_average"]
+
+    data = data.resample(new_resolution, "time")
+    # data["time"] = data["time"].astype(datetime)
+    return data
 
 
 def plot_temperature(ax, start, end, datasets, config):
@@ -60,27 +75,22 @@ def plot_temperature(ax, start, end, datasets, config):
     Returns:
         None
     """
-    data = datasets["DShip"].accumulate(start, end)
-    data = data.limit_by("time", start, end)
-    data = data.sort_by("time")
-
+    data = sample(datasets[config["General"]["metadata"]], config, start, end)
     point_size = int(config["Plots"]["point_size"])
-    bin_size = get_bin_size(len(data["time"]), config)
 
     ax.scatter(
-        data["time"].average(bin_size),
-        data["air_temperature"].average(bin_size),
+        data["time"].data, data["air_temperature"],
         s=point_size, label="Air")
     ax.scatter(
-        data["time"].average(bin_size),
-        data["water_temperature"].average(bin_size),
+        data["time"].data,
+        data["water_temperature"],
         s=point_size, label="Water")
     ax.legend()
     ax.set_ylabel("Temperature")
 
 
 def plot_pressure(ax, start, end, datasets, config):
-    """Plot DShip  data on matplotlib axis.
+    """Plot DShip data on matplotlib axis.
 
     Args:
         ax: A matplotlib.axis object.
@@ -92,16 +102,12 @@ def plot_pressure(ax, start, end, datasets, config):
     Returns:
         None
     """
-    data = datasets["DShip"].accumulate(start, end)
-    data = data.limit_by("time", start, end)
-    data = data.sort_by("time")
-
+    data = sample(datasets[config["General"]["metadata"]], config, start, end)
     point_size = int(config["Plots"]["point_size"])
-    bin_size = get_bin_size(len(data["time"]), config)
 
     ax.scatter(
-        data["time"].average(bin_size),
-        data["air_pressure"].average(bin_size),
+        data["time"].data,
+        data["air_pressure"],
         s=point_size,
     )
     ax.set_ylabel("Pressure")
@@ -120,16 +126,12 @@ def plot_humidity(ax, start, end, datasets, config):
     Returns:
         None
     """
-    data = datasets["DShip"].accumulate(start, end)
-    data = data.limit_by("time", start, end)
-    data = data.sort_by("time")
-
+    data = sample(datasets[config["General"]["metadata"]], config, start, end)
     point_size = int(config["Plots"]["point_size"])
-    bin_size = get_bin_size(len(data["time"]), config)
 
     ax.scatter(
-        data["time"].average(bin_size).astype("O"),
-        data["humidity"].average(bin_size),
+        data["time"].data,
+        data["humidity"],
         s=point_size
     )
     ax.set_ylabel("Humidity")
@@ -148,26 +150,22 @@ def plot_cloud_coverage(ax, start, end, datasets, config, instrument):
     Returns:
         None
     """
-    data = datasets[instrument+"-stats"].accumulate(start, end)
-    data = data.limit_by("time", start, end)
-    data = data.sort_by("time")
-
+    data = sample(datasets[instrument+"-stats"], config, start, end)
     point_size = int(config["Plots"]["point_size"])
-    bin_size = get_bin_size(len(data["time"]), config)
 
     ax.scatter(
-        data["time"].average(bin_size),
-        data["cloud_coverage"][:, 0].average(bin_size),
+        data["time"].data,
+        data["cloud_coverage"][:, 0],
         s=point_size
     )
     ax.scatter(
-        data["time"].average(bin_size),
-        data["cloud_coverage"][:, 1].average(bin_size),
+        data["time"].data,
+        data["cloud_coverage"][:, 1],
         s=point_size, c="r"
     )
     ax.scatter(
-        data["time"].average(bin_size),
-        data["cloud_coverage"][:, 2].average(bin_size),
+        data["time"].data,
+        data["cloud_coverage"][:, 2],
         s=point_size, c="k"
     )
     ax.legend(["Low", "Middle", "High"])
@@ -213,28 +211,23 @@ def plot_ceilometer(ax, start, end, datasets, config):
         None
     """
 
-    data = datasets["Ceilometer"].accumulate(
-        start, end, fields=("cbh", "time")
-    )
-    data = data.limit_by("time", start, end)
-    data = data.sort_by("time")
-
+    data = sample(datasets["Ceilometer"], config, start, end,
+                  fields=["cbh", "time"],)
     point_size = int(config["Plots"]["point_size"])
-    bin_size = get_bin_size(len(data["time"]), config)
 
     ax.scatter(
-        data["time"].average(bin_size),
-        data["cbh"][:, 0].average(bin_size),
+        data["time"].data,
+        data["cbh"][:, 0],
         s=point_size,
     )
     ax.scatter(
-        data["time"].average(bin_size),
-        data["cbh"][:, 1].average(bin_size),
+        data["time"].data,
+        data["cbh"][:, 1],
         s=point_size
     )
     ax.scatter(
-        data["time"].average(bin_size),
-        data["cbh"][:, 2].average(bin_size),
+        data["time"].data,
+        data["cbh"][:, 2],
         s=point_size
     )
     ax.legend(["Layer 1", "Layer 2", "Layer 3"])
@@ -255,7 +248,7 @@ def plot_overview(datasets, config, start, end, ):
         None
     """
 
-    print("Plot overview from %s to %s" % (start, end))
+    logging.info("Plot overview from %s to %s" % (start, end))
 
     plt.rcParams.update({'font.size': 12})
 
@@ -270,6 +263,7 @@ def plot_overview(datasets, config, start, end, ):
 
     fig, axes = plt.subplots(
         len(plots), sharex=True, figsize=(16, 12), dpi=300)
+    no_data = True
     for i, plot in enumerate(plots.items()):
         name, plot_func = plot
 
@@ -277,11 +271,16 @@ def plot_overview(datasets, config, start, end, ):
         # abort the whole script:
         try:
             plot_func(axes[i], start, end, datasets, config)
-        except Exception as err:
-            logger.error("Could not create %s plot:" % name, exc_info=True)
-            traceback.print_tb(err.__traceback__)
-
+            no_data = False
+        except cloud.NoFilesError as err:
+            logging.warning(str(err))
+        except Exception:
+            logging.error("Could not create %s plot:" % name, exc_info=True)
         axes[i].grid()
+
+    if no_data:
+        logging.info("Skip this plot because there is no data!")
+        return
 
     axes[0].set_title("Overview from %s to %s" % (
         start.to_pydatetime(), end.to_pydatetime()))
@@ -292,58 +291,28 @@ def plot_overview(datasets, config, start, end, ):
     ])
     fig.tight_layout()
 
-    path = datasets["plot-overview"].generate_filename(
-        datasets["plot-overview"].files, start, end)
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    print("  Save to %s" % path)
-
     # Save the generated plot:
-    fig.savefig(path)
+    path = datasets["plot-overview"].generate_filename((start, end))
+    logging.info("Save plot to %s" % path)
+    datasets["plot-overview"].write(path, fig)
 
 
 def plot_four_statistics(axes, data, config, instrument, add_labels=False):
     point_size = int(config["Plots"]["point_size"])
 
-    data_vars = set(data.vars())
-    data_vars.remove("time")
-    data_vars = list(data_vars)
+    data_vars = iter(data.data_vars.values())
     for i, ax in enumerate(axes.flatten()):
+        var = next(data_vars)
         if add_labels:
-            ax.set_title(data[data_vars[i]].attrs["description"])
-            ax.set_ylabel(data[data_vars[i]].attrs["units"])
+            ax.set_title(var.attrs["description"])
+            ax.set_ylabel(var.attrs["units"])
 
         ax.scatter(
-            data["time"],
-            data[data_vars[i]],
+            data["time"].data,
+            var,
             s=point_size,
             label=instrument,
         )
-
-
-    # axes[0, 0].scatter(
-    #     data["time"],
-    #     data["cloud_coverage"].sum(axis=1),
-    #     s=point_size,
-    #     label=instrument,
-    # )
-    # axes[0, 1].scatter(
-    #     data["time"],
-    #     np.nanmean(data["cloud_mean_temperature"], axis=1),
-    #     s=point_size,
-    #     label=instrument,
-    # )
-    # axes[1, 0].scatter(
-    #     data["time"],
-    #     np.nanmin(data["cloud_min_temperature"], axis=1),
-    #     s=point_size,
-    #     label=instrument,
-    # )
-    # return axes[1, 1].scatter(
-    #     data["time"],
-    #     np.nanmax(data["cloud_max_temperature"], axis=1),
-    #     s=point_size,
-    #     label=instrument,
-    # )
 
 
 def _prepare_parameters(data):
@@ -355,18 +324,18 @@ def _prepare_parameters(data):
     Returns:
         None
     """
-    data["total_coverage"] = data["cloud_coverage"].sum(axis=1)
-    del data["cloud_coverage"]#
+    data["total_coverage"] = data["cloud_coverage"].sum(dim="level")
+    del data["cloud_coverage"]
     if "inhomogeneity" in data:
         del data["inhomogeneity"]
     if "cloud_inhomogeneity" in data:
         del data["cloud_inhomogeneity"]
     data["cloud_mean_temperature"] = \
-        np.nanmean(data["cloud_mean_temperature"], axis=1)
+        data["cloud_mean_temperature"].mean(dim="level")
     data["cloud_min_temperature"] = \
-        np.nanmin(data["cloud_min_temperature"], axis=1)
+        data["cloud_min_temperature"].min(dim="level")
     data["cloud_max_temperature"] = \
-        np.nanmax(data["cloud_max_temperature"], axis=1)
+        data["cloud_max_temperature"].max(dim="level")
 
     return data
 
@@ -384,7 +353,7 @@ def plot_comparison(datasets, config, start, end, ptype):
     Returns:
         None
     """
-    logger.info("Plot %s from %s to %s" % (ptype, start, end))
+    logging.info("Plot %s from %s to %s" % (ptype, start, end))
 
     plt.rcParams.update({'font.size': 15})
     fig, axes = plt.subplots(
@@ -392,16 +361,25 @@ def plot_comparison(datasets, config, start, end, ptype):
     )
 
     instruments = ["Pinocchio", "Dumbo"]
+    no_data = True
     for instrument in instruments:
         try:
-            data = datasets[instrument+"-stats"].accumulate(start, end)
-            data = data.limit_by("time", start, end)
-            data = data.sort_by("time")
+            data = sample(
+                datasets[instrument+"-stats"], config, start, end
+            )
             data = _prepare_parameters(data)
 
             plot_four_statistics(axes, data, config, instrument)
+            no_data = False
+        except cloud.NoFilesError as err:
+            logging.warning(str(err))
         except Exception as err:
-            logger.error("Could not plot %s!" % instrument, exc_info=True)
+            logging.error("Could not create %s plot:" % instrument,
+                          exc_info=True)
+
+    if no_data:
+        logging.info("Skip this plot because there is no data!")
+        return
 
     fig.suptitle("%s %s (%s - %s)" % (
         ptype.capitalize(), "-".join(instruments), start, end
@@ -422,13 +400,10 @@ def plot_comparison(datasets, config, start, end, ptype):
     handles, labels = axes[1, 1].get_legend_handles_labels()
     fig.legend(handles, labels, 'upper left')
 
-    path = datasets["plot-"+ptype].generate_filename(
-        datasets["plot-"+ptype].files, start, end)
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    print("  Save to %s" % path)
-
     # Save the generated plot:
-    fig.savefig(path,)
+    path = datasets["plot-comparison"].generate_filename((start, end))
+    logging.info("Save plot to %s" % path)
+    datasets["plot-comparison"].write(path, fig)
 
 
 def get_cmd_line_parser():
@@ -444,14 +419,12 @@ def get_cmd_line_parser():
     examples = """
 Examples:
 
-    > %(prog)s -xcs "2017-11-02" "2017-11-03"
-    Recommended usage: the program extracts the raw files from the 2nd November
-    2017 (only necessary for Pinocchio files), converts them to netCDF files 
-    and calculates the cloud statistics. If [instrument][mask] is set to a 
-    valid filename, a mask will be applied on all images.
-
     > ./%(prog)s -o "2017-11-03" "2017-11-10"
     Plot an overview of all instruments from 2017-11-02 to 2017-11-10.
+    
+    > ./%(prog)s -c "2017-11-03" "2017-11-04"
+    Plot a comparison of Dumbo and Pinocchio instrument from the 3rd of 
+    November 2017.
 
     > ./%(prog)s -of 3H "2017-11-03" "2017-11-10"
     Same as above but instead of creating one plot for the full time period,
@@ -488,7 +461,7 @@ Examples:
     )
     parser.add_argument(
         '-a', '--anomaly', action='store_true',
-        help='Note: Does not work at the moment! Create anomaly plots for '
+        help='Note: DEPRECATED at the moment! Create anomaly plots for '
              'Dumbo and Pinocchio. Saves the plots in the path that is set by '
              '[Plots][anomaly] config option.'
     )
